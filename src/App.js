0e05ee27b299cf7b5391edb19eefc8dd298acb02
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-09-12 22:09:31 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,95 +1,72 @@
-import React, { Component } from 'react';
-import Location from './components/Location';
-import SearchForm from './components/SearchForm';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/first.css'
-
-
+import React, { Component } from "react";
+import Location from "./components/Location";
+import SearchForm from "./components/SearchForm";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/first.css";
 
 export class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      city_name:"",
-      lat:"",
-      lon:"",
-      lat_lon:'',
-      map:'',
-      showData:false,
-      iframe:''
-    }
+    this.state = {
+      city_name: "",
+      lat: "",
+      lon: "",
+      lat_lon: "",
+      map: "",
+      showData: false,
+      iframe: "",
+    };
   }
-  handleLocation=(e)=>{
-    let city_name=e.target.value;
+  handleLocation = (e) => {
+    let city_name = e.target.value;
     this.setState({
-      city_name:city_name
-    })
-  }
-  handleSubmit=(e)=>{
+      city_name: city_name,
+    });
+  };
+  handleSubmit = (e) => {
     e.preventDefault();
-    let config={
-      method:"GET",
-      baseURL:`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json `
-      
-    }
-    
-    axios(config).then(res=>{
-      let responseData=res.data[0]
-      // console.log(responseData);
-      this.setState({
-        city_name:responseData.display_name,
-        lon:responseData.lon,
-        lat:responseData.lat,
-        lat_lon:`${responseData.lat.toString()},${responseData.lon.toString()}`,
-        showData:true,
-        iframe:'iframe'
+    let config = {
+      method: "GET",
+      baseURL: `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json `,
+    };
 
-      })
-      let configmap ={
-        method:"GET",
-        baseURL:`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18
-        &center=${this.state.lat_lon} `
-  
-  
-      }
-      console.log(this.state.lat_lon);
-      axios(configmap).then(res2=>{
-        let map = res2.config.baseURL
-        console.log(map);
-        console.log(res2);
-        this.setState({
-          map:map
-          
-      })
-      
-    })
-    })
-   
-}
+    axios(config).then((res) => {
+      let responseData = res.data[0];
+      console.log(responseData);
+      this.setState({
+        city_name: responseData.display_name,
+        lon: responseData.lon,
+        lat: responseData.lat,
+        lat_lon: `${responseData.lat.toString()},${responseData.lon.toString()}`,
+        showData: true,
+        iframe: "iframe",
+      });
+    });
+  };
   render() {
     return (
       <div>
         <h1>Welcome to City explorer</h1>
-        <SearchForm handleLocation={this.handleLocation} handleSubmit={this.handleSubmit}/>
-        {
-          this.state.showData&&
-          <Location city_name={this.state.city_name}
-                    type={this.state.type}
-                    lat={this.state.lat}
-                    lon={this.state.lon}
-                    map={this.state.map}
-                    iframe={this.state.iframe}
-
+        <SearchForm
+          handleLocation={this.handleLocation}
+          handleSubmit={this.handleSubmit}
+        />
+        {this.state.showData && (
+          <Location
+            city_name={this.state.city_name}
+            type={this.state.type}
+            lat={this.state.lat}
+            lon={this.state.lon}
+            map={this.state.map}
+            iframe={this.state.iframe}
+            img={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18
+                    &center=${this.state.lat_lon}`}
           />
-        }
-         <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18
-        &center=${this.state.lat_lon}`} 
-   alt='' />
-        
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
