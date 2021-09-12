7682353c7acf9954647233ca5330fd68f3fd164d
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-09-12 18:09:48 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Location from './components/Location';
 import SearchForm from './components/SearchForm';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/first.css'
+
+
 
 export class App extends Component {
   constructor(props){
@@ -10,7 +14,10 @@ export class App extends Component {
       city_name:"",
       lat:"",
       lon:"",
-      showData:false
+      lat_lon:'',
+      map:'',
+      showData:false,
+      iframe:''
     }
   }
   handleLocation=(e)=>{
@@ -20,25 +27,46 @@ export class App extends Component {
     })
   }
   handleSubmit=(e)=>{
-    console.log(`${process.env.REACT_APP_LOCATIONIQ_API_KEY}`);
     e.preventDefault();
     let config={
       method:"GET",
-      baseURL:`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json`
+      baseURL:`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json `
       
     }
+    
     axios(config).then(res=>{
       let responseData=res.data[0]
-      console.log(responseData);
+      // console.log(responseData);
       this.setState({
         city_name:responseData.display_name,
         lon:responseData.lon,
         lat:responseData.lat,
-        showData:true
+        lat_lon:`${responseData.lat.toString()},${responseData.lon.toString()}`,
+        showData:true,
+        iframe:'iframe'
 
       })
+      let configmap ={
+        method:"GET",
+        baseURL:`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18
+        &center=${this.state.lat_lon} `
+  
+  
+      }
+      console.log(this.state.lat_lon);
+      axios(configmap).then(res2=>{
+        let map = res2.config.baseURL
+        console.log(map);
+        console.log(res2);
+        this.setState({
+          map:map
+          
+      })
+      
     })
-  }
+    })
+   
+}
   render() {
     return (
       <div>
@@ -50,9 +78,13 @@ export class App extends Component {
                     type={this.state.type}
                     lat={this.state.lat}
                     lon={this.state.lon}
+
           />
         }
-        
+         
+         <div className="ratio ratio-16x9 div_iframe">
+      <iframe src={this.state.map}  title="country map" allowfullscreen className={this.state.iframe}></iframe>
+    </div>
       </div>
     )
   }
--- src/components/Location.js
+++ src/components/Location.js
@@ -4,8 +4,27 @@ export class Location extends Component {
     render() {
         return (
             <div>
-                <h1>{this.props.city_name}</h1>
-                <h3>{this.props.lat}/{this.props.lon}</h3>
+                
+
+                <table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">city name</th>
+      <th scope="col">longitude</th>
+      <th scope="col">latitude</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>{this.props.city_name}</td>
+      <td>{this.props.lat}</td>
+      <td>{this.props.lon}</td>
+    </tr>
+   
+  </tbody>
+</table>
 
             </div>
         )
