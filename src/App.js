5253b66495c8003d279767dfc910c8da7ebaa5d9
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-09-12 15:44:57 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Location from './components/Location';
+import SearchForm from './components/SearchForm';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      city_name:"",
+      lat:"",
+      lon:"",
+      showData:false
+    }
+  }
+  handleLocation=(e)=>{
+    let city_name=e.target.value;
+    this.setState({
+      city_name:city_name
+    })
+  }
+  handleSubmit=(e)=>{
+    console.log(`${process.env.REACT_APP_LOCATIONIQ_API_KEY}`);
+    e.preventDefault();
+    let config={
+      method:"GET",
+      baseURL:`https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}`
+    }
+    axios(config).then(res=>{
+      let responseData=res.data[0]
+      console.log(responseData);
+      this.setState({
+        city_name:responseData.display_name,
+        lon:responseData.lon,
+        lat:responseData.lat,
+        showData:true
+
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to City explorer</h1>
+        <SearchForm handleLocation={this.handleLocation} handleSubmit={this.handleSubmit}/>
+        {
+          this.state.showData&&
+          <Location city_name={this.state.city_name}
+                    type={this.state.type}
+                    lat={this.state.lat}
+                    lon={this.state.lon}
+          />
+        }
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
