2bffdca2f94dbb94791b4c9cc7f6d067d2aca81c
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-09-14 20:57:22 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./components/first.css";
 import { Alert } from "react-bootstrap";
 import Weather from "./components/Weather";
-
+import Movie from "./components/Movie";
 export class App extends Component {
   constructor(props) {
     super(props);
@@ -18,47 +18,75 @@ export class App extends Component {
       map: "",
       showData: false,
       iframe: "",
-      error:'',
-      errorhandle:false,
-      weather:[],
-      name_weather:[],
-      err:''
+      error: "",
+      errorhandle: false,
+      weather: [],
+      name_weather: [],
+      err: "",
+      overview: [],
+      originalname: "",
     };
   }
   handleLocation = (e) => {
     let city_name = e.target.value;
     this.setState({
       city_name: city_name,
+      
     });
   };
   handleSubmit = (e) => {
     e.preventDefault();
+    this.setState({
+      originalname: this.state.city_name,
+    })
     let config = {
       method: "GET",
       baseURL: `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json `,
     };
 
-    axios(config).then((res) => {
-      let responseData = res.data[0];
-      this.setState({
-        city_name: responseData.display_name,
-        lon: responseData.lon,
-        lat: responseData.lat,
-        lat_lon: `${responseData.lat.toString()},${responseData.lon.toString()}`,
-        showData: true,
-        iframe: "iframe",
-        errorhandle:false,
-        errorhand:false
-      });
-    }).catch(e=>{this.setState({error:e.toString(),errorhandle:true})}).then(()=>
-    axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}`))
-    .then(res=>{
-      this.setState({
-        weather:res.data.foreCast,
-        name_weather:res.data.city_name
+    axios(config)
+      .then((res) => {
+        let responseData = res.data[0];
+        this.setState({
+          city_name: responseData.display_name,
+          lon: responseData.lon,
+          lat: responseData.lat,
+          lat_lon: `${responseData.lat.toString()},${responseData.lon.toString()}`,
+          showData: true,
+          iframe: "iframe",
+          errorhandle: false,
+          errorhand: false,
+        });
       })
-    }).catch(e=>{this.setState({err:e.toString(),errorhand:true})})
-  }
+      .catch((e) => {
+        this.setState({ error: e.toString(), errorhandle: true });
+      })
+      .then(() =>
+        axios.get(
+          `http://${process.env.REACT_APP_BACKEND_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}`
+        )
+      )
+      .then((res) => {
+        this.setState({
+          weather: res.data.foreCast,
+          name_weather: res.data.city_name,
+        });
+      })
+      .catch((e) => {
+        this.setState({ err: e.toString(), errorhand: true });
+      })
+      .then(() =>
+        axios.get(
+          `http://${process.env.REACT_APP_BACKEND_URL}/movie?query=${this.state.originalname}`
+        )
+      )
+      .then((res) => {
+        this.setState({
+          overview: res.data,
+        });
+        // console.log(this.state.overview[0].title);
+      });
+  };
   // catch(e=>{this.setState({error:e.toString(),errorhandle:true})})
   render() {
     return (
@@ -68,7 +96,7 @@ export class App extends Component {
           handleLocation={this.handleLocation}
           handleSubmit={this.handleSubmit}
         />
-        {this.state.showData && this.state.errorhandle===false && (
+        {this.state.showData && this.state.errorhandle === false && (
           <Location
             city_name={this.state.city_name}
             type={this.state.type}
@@ -79,25 +107,22 @@ export class App extends Component {
             img={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18
                     &center=${this.state.lat_lon}`}
           />
-
         )}
-        {this.state.errorhand===false &&(
-        <Weather forcast = {this.state.weather} name = {this.state.name_weather}/>)}
+        {this.state.errorhand === false && (
+          <Weather
+            forcast={this.state.weather}
+            name={this.state.name_weather}
+          />
+        )}
 
+        <Movie movielist={this.state.overview}/>
+        
 
+        {this.state.errorhandle && (
+          <Alert variant="dark">{this.state.error}</Alert>
+        )}
 
-       {this.state.errorhandle &&( <Alert variant='dark'>
-       {this.state.error} 
-
-  </Alert>)}
-
-       {this.state.errorhand &&( <Alert variant='dark'>
-       {this.state.err} 
-
-  </Alert>)}
-
-
- 
+        {this.state.errorhand && <Alert variant="dark">{this.state.err}</Alert>}
       </div>
     );
   }
