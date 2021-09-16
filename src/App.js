43296146105673037d33b9ad9ced70875787144c
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-09-16 17:35:56 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,134 +1,84 @@
-import React, { Component } from "react";
-import Location from "./components/Location";
+import Locationdata from "./components/Locationdata";
+import Weatherdata from "./components/Weatherdata";
 import SearchForm from "./components/SearchForm";
+import Moviedata from "./components/Moviedata"
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./components/first.css";
-import { Alert } from "react-bootstrap";
-import Weather from "./components/Weather";
-import Movie from "./components/Movie";
+import React, { Component } from "react";
 
-
-export class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      city_name: "",
-      lat: "",
-      lon: "",
-      lat_lon: "",
-      map: "",
-      showData: false,
-      iframe: "",
-      error: "",
-      errorhandle: false,
-      weather: [],
-      name_weather: [],
-      err: "",
-      overview: [],
-      originalname: "",
+      errorMsg: "",
+      cityName: "",
+      locationData: [],
+      locationdisplay: true,
+      weatherData: [],
+      weatherdisplay: true,
+      movieData: [],
+      moviedisplay: true,
     };
   }
   handleLocation = (e) => {
     let city_name = e.target.value;
     this.setState({
-      city_name: city_name,
-      
+      cityName: city_name,
     });
   };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({
-      originalname: this.state.city_name,
-    })
-    let config = {
-      method: "GET",
-      baseURL: `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json `,
-    };
 
-    axios(config)
-      .then((res) => {
-        let responseData = res.data[0];
-        this.setState({
-          city_name: responseData.display_name,
-          lon: responseData.lon,
-          lat: responseData.lat,
-          lat_lon: `${responseData.lat.toString()},${responseData.lon.toString()}`,
-          showData: true,
-          iframe: "iframe",
-          errorhandle: false,
-          errorhand: false,
-        });
-      })
-      .catch((e) => {
-        this.setState({ error: e.toString(), errorhandle: true });
-      })
-      .then(() =>
-        axios.get(`${process.env.REACT_APP_API_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}`
-          
-        )
-      )
-      .then((res) => {
-        this.setState({
-          weather: res.data.foreCast,
-          name_weather: res.data.city_name,
-        });
-      })
-      .catch((e) => {
-        this.setState({ err: e.toString(), errorhand: true });
-      })
-      .then(() =>
-        axios.get(
-          `${process.env.REACT_APP_API_URL}/movie?query=${this.state.originalname}`
-        )
-      )
-      .then((res) => {
-        this.setState({
-          overview: res.data,
-        });
-        // console.log(this.state.overview[0].title);
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // start call API
+      let originalName = this.state.cityName;
+      let location = await axios.get(
+        `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${originalName}&format=json `
+      );
+      //######################
+      let lat = location.data[0].lat;
+      let lon = location.data[0].lon;
+      let weather = await axios.get(
+        `${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${lon}`
+      );
+      //######################
+      let movie = await axios.get(
+        `${process.env.REACT_APP_API_URL}/movie?query=${originalName}`
+      );
+      // end call API
+      // start store API data
+      this.setState({
+        locationData: location.data[0],
+        errorMsg: "",
+        weatherData: weather.data.foreCast,
+        movieData: movie.data,
+        displayLocation: false,
       });
+      //end store API data
+    } catch (error) {
+      //start catch error
+      this.setState({
+        errorMsg: error.message,
+        // displayLocation: false,
+        // showWeather: false,
+        // showMovie: false,
+        movieData: error.response,
+      });
+    }
   };
-  // catch(e=>{this.setState({error:e.toString(),errorhandle:true})})
   render() {
     return (
       <div>
-        <h1>Welcome to City explorer</h1>
         <SearchForm
           handleLocation={this.handleLocation}
           handleSubmit={this.handleSubmit}
         />
-        {this.state.showData && this.state.errorhandle === false && (
-          <Location
-
-          city_name={this.state.city_name}
-            type={this.state.type}
-            lat={this.state.lat}
-            lon={this.state.lon}
-            map={this.state.map}
-            iframe={this.state.iframe}
-            img={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18
-                    &center=${this.state.lat_lon}`}
-          />
-        )}
-        {this.state.errorhand === false && (
-          <Weather
-            forcast={this.state.weather}
-            name={this.state.name_weather}
-          />
-        )}
-
-        <Movie movielist={this.state.overview}/>
-        
-
-        {this.state.errorhandle && (
-          <Alert variant="dark">{this.state.error}</Alert>
-        )}
-
-        {this.state.errorhand && <Alert variant="dark">{this.state.err}</Alert>}
+        <Locationdata location={this.state.locationData}/>
+        <Weatherdata weather={this.state.weatherData}/>
+        <Moviedata movie={this.state.movieData}/>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/example.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import Location from "./components/Location";
+import SearchForm from "./components/SearchForm";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/first.css";
+import { Alert } from "react-bootstrap";
+import Weather from "./components/Weather";
+import Movie from "./components/Movie";
+
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city_name: "",
+      lat: "",
+      lon: "",
+      lat_lon: "",
+      map: "",
+      showData: false,
+      iframe: "",
+      error: "",
+      errorhandle: false,
+      weather: [],
+      name_weather: [],
+      err: "",
+      overview: [],
+      originalname: "",
+    };
+  }
+  handleLocation = (e) => {
+    let city_name = e.target.value;
+    this.setState({
+      city_name: city_name,
+      
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      originalname: this.state.city_name,
+    })
+    let config = {
+      method: "GET",
+      baseURL: `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json `,
+    };
+
+    axios(config)
+      .then((res) => {
+        let responseData = res.data[0];
+        this.setState({
+          city_name: responseData.display_name,
+          lon: responseData.lon,
+          lat: responseData.lat,
+          lat_lon: `${responseData.lat.toString()},${responseData.lon.toString()}`,
+          showData: true,
+          iframe: "iframe",
+          errorhandle: false,
+          errorhand: false,
+        });
+      })
+      .catch((e) => {
+        this.setState({ error: e.toString(), errorhandle: true });
+      })
+      .then(() =>
+        axios.get(`${process.env.REACT_APP_API_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}`
+          
+        )
+      )
+      .then((res) => {
+        this.setState({
+          weather: res.data.foreCast,
+          name_weather: res.data.city_name,
+        });
+      })
+      .catch((e) => {
+        this.setState({ err: e.toString(), errorhand: true });
+      })
+      .then(() =>
+        axios.get(
+          `${process.env.REACT_APP_API_URL}/movie?query=${this.state.originalname}`
+        )
+      )
+      .then((res) => {
+        this.setState({
+          overview: res.data,
+        });
+      });
+  };
+  // catch(e=>{this.setState({error:e.toString(),errorhandle:true})})
+  render() {
+    return (
+      <div>
+        <h1>Welcome to City explorer</h1>
+        <SearchForm
+          handleLocation={this.handleLocation}
+          handleSubmit={this.handleSubmit}
+        />
+        {this.state.showData && this.state.errorhandle === false && (
+          <Location
+
+          city_name={this.state.city_name}
+            type={this.state.type}
+            lat={this.state.lat}
+            lon={this.state.lon}
+            map={this.state.map}
+            iframe={this.state.iframe}
+            img={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18
+                    &center=${this.state.lat_lon}`}
+          />
+        )}
+        {this.state.errorhand === false && (
+          <Weather
+            forcast={this.state.weather}
+            name={this.state.name_weather}
+          />
+        )}
+
+        <Movie movielist={this.state.overview}/>
+        
+
+        {this.state.errorhandle && (
+          <Alert variant="dark">{this.state.error}</Alert>
+        )}
+
+        {this.state.errorhand && <Alert variant="dark">{this.state.err}</Alert>}
+      </div>
+    );
+  }
+}
+
+export default App;
